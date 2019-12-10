c5ee326e1332bfbde6cb565429024ba597f1768e
Jacob <jtljac2@gmail.com>
2019-12-10 18:05:50 +0000

    Added mobile navbar, occurs when the screensize gets too small

--- Javascript/autoResize.js
+++ Javascript/autoResize.js
@@ -1,6 +1,7 @@
 var previousWidth = window.innerWidth;
 var currentItem = -1;
 var useMore = false;
+var wasMobile = false;
 const sizes = {
 	"Overview":120,
 	"Design":205,
@@ -45,53 +46,65 @@ function createDropDown(){
 function resizeElements(link){
 	var width = window.innerWidth;
 	if(previousWidth != width || currentItem == -1){
-		var possibleLength = currentItem;
-		var alsoMore = useMore
-		if(width > previousWidth || possibleLength == -1){
-			var foundSize = false;
-			while (!foundSize){
-				if (possibleLength == 6){
-					alsoMore = false;
-					foundSize = true;
-				} else if (sizes[Object.keys(sizes)[possibleLength + 1]] <= width){
-					possibleLength++
-				} else if (sizes[Object.keys(sizes)[possibleLength]] + moreSize <= width && possibleLength != 5){
-					foundSize = true;
-					alsoMore = true;
-				} else if (possibleLength == 0){
-					alsoMore = true;
-					foundSize = true;
-				} else {
-					possibleLength--;
-					foundSize = true;
-					alsoMore = true;
-				}
-			}
-		} else if (width < previousWidth){
-			var foundSize = false;
-			while (!foundSize){
-				if (possibleLength == 0){
-					alsoMore = true;
-					foundSize = true;
-				} else if (sizes[Object.keys(sizes)[possibleLength]] > width){
-					possibleLength--;
-				} else if (sizes[Object.keys(sizes)[possibleLength]] + moreSize <= width && possibleLength != 6 && possibleLength != 5){
-					alsoMore = true;
-					foundSize = true;
-				} else if (possibleLength == 5){
-					possibleLength = 4
-					alsoMore = true;
-					foundSize = true;
-				} else if (possibleLength == 6){
-					alsoMore = false;
-					foundSize = true;
-				} else {
-					possibleLength--;
-					alsoMore = true;
-					foundSize = true;
-				}
-			}
+		var possibleLength;
+		if (wasMobile){
+			possibleLength = 0
+			wasMobile = false;
+		} else {
+			possibleLength = currentItem;
 		}
+		var alsoMore = useMore
+		if (width > 480 ){
+			if(width > previousWidth || possibleLength == -1){
+				var foundSize = false;
+				while (!foundSize){
+					if (possibleLength == 6){
+						alsoMore = false;
+						foundSize = true;
+					} else if (sizes[Object.keys(sizes)[possibleLength + 1]] <= width){
+						possibleLength++
+					} else if (sizes[Object.keys(sizes)[possibleLength]] + moreSize <= width && possibleLength != 5){
+						foundSize = true;
+						alsoMore = true;
+					} else if (possibleLength == 0){
+						alsoMore = true;
+						foundSize = true;
+					} else {
+						possibleLength--;
+						foundSize = true;
+						alsoMore = true;
+					}
+				}
+			} else if (width < previousWidth){
+				var foundSize = false;
+				while (!foundSize){
+					if (possibleLength == 0){
+						alsoMore = true;
+						foundSize = true;
+					} else if (sizes[Object.keys(sizes)[possibleLength]] > width){
+						possibleLength--;
+					} else if (sizes[Object.keys(sizes)[possibleLength]] + moreSize <= width && possibleLength != 6 && possibleLength != 5){
+						alsoMore = true;
+						foundSize = true;
+					} else if (possibleLength == 5){
+						possibleLength = 4
+						alsoMore = true;
+						foundSize = true;
+					} else if (possibleLength == 6){
+						alsoMore = false;
+						foundSize = true;
+					} else {
+						possibleLength--;
+						alsoMore = true;
+						foundSize = true;
+					}
+				}
+			}
+		} else {
+			possibleLength = 6;
+			alsoMore = false;
+			wasMobile = true;
+		}	
 
 		var navbar = document.getElementById("navbar");
 		navbar.innerHTML = "";
