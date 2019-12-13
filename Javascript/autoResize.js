35ea01765249fbed4b0c3a1dedcf69506a396eeb
Jacob <jtljac2@gmail.com>
2019-12-13 13:51:29 +0000

    Added Ui and tech pages

--- Javascript/autoResize.js
+++ Javascript/autoResize.js
@@ -18,7 +18,7 @@ const items = [
 	["#Technology", "../199045436/Technology.html", "Technology"],
 	["#Exclusives", "../199021534/Exclusives.HTML", "Exclusives"],
 	["#Market","../199028083/Market.html", "Market Distribution"],
-	["#UIUX", "..//UIUX.html", "UI/UX"]
+	["#UIUX", "../199027659/UIUX.html", "UI/UX"]
 ]
   
   
